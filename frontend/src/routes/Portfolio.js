0d4fb73cb2c36014b44c3ff48cb41e0a5fcb9c3a
smaristeinar <smairkul@hotmail.com>
2022-09-17 22:34:07 +0200

    feat: add get to know me button

--- frontend/src/routes/Portfolio.js
+++ frontend/src/routes/Portfolio.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getPortFolioItems } from "../Helpers/getPortfolioItems";
 import Card from "../components/Card";
+import Button from "../components/Button";
 
 export default function Portfolio() {
   const [portfolioItems, setPortfolioItems] = useState([]);
@@ -11,7 +12,7 @@ export default function Portfolio() {
   }, []);
 
   return (
-    <section className="md:flex md:justify-center">
+    <section className="flex flex-col items-center">
       <div className="flex flex-col items-center pt-20 pb-20 lg:p-20 lg:gap-16 lg:items-start">
         <h1 className="text-3xl pb-10 font-bold lg:pb-0">Portfolio</h1>
         {portfolioItems?.data?.length ? (
@@ -21,6 +22,9 @@ export default function Portfolio() {
         ) : (
           ""
         )}
+      </div>
+      <div className="pb-6">
+        <Button content={"Get to know me"} />
       </div>
     </section>
   );
